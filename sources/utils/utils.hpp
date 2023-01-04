3e3abce32f949c3000044d12aed9857a0fd17020
gkehren <kehren.guillaume@gmail.com>
2023-01-04 20:10:28 +0100

    [ADD] utils

---
+++ sources/utils/utils.hpp
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gkehren <gkehren@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 15:00:07 by gkehren           #+#    #+#             */
+/*   Updated: 2023/01/04 19:22:56 by gkehren          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include "iterator.hpp"
+
+namespace ft
+{
+	template<bool, typename T = void>
+	struct enable_if {};
+
+	template<typename T>
+	struct enable_if<true, T> { typedef T type; };
+
+	template<class T, T v>
+	struct is_integral_res
+	{
+		static const T value = v;
+		operator T() const { return value; }
+	};
+
+	template<class T>
+	struct is_integral: is_integral_res<bool, std::numeric_limits<T>::is_integer> {};
+
+	template<class InputIt1, class InputIt2>
+	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
+	{
+		for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+		{
+			if (*first1 < *first2)
+				return true;
+			if (*first2 < *first1)
+				return false;
+		}
+		return (first1 == last1) && (first2 != last2);
+	}
+
+	template<class InputIt1, class InputIt2, class Compare>
+	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Compare comp)
+	{
+		for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+		{
+			if (comp(*first1, *first2))
+				return true;
+			if (comp(*first2, *first1))
+				return false;
+		}
+		return (first1 == last1) && (first2 != last2);
+	}
+
+	template<class InputIt1, class InputIt2>
+	bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2)
+	{
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!(*first1 == *first2)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	template<class InputIt1, class InputIt2, class BinaryPredicate>
+	bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p)
+	{
+		for (; first1 != last1; ++first1, ++first2) {
+			if (!p(*first1, *first2)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	template<class T>
+	void	swap(T & a, T & b)
+	{
+		T tmp = a;
+		a = b;
+		b = tmp;
+	}
+}
+
+
+#endif
